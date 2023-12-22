0c93a22604cf6c5a7b33fd124fc1764f56871080
Alex <alexander.prohnitchi@gmail.com>
2023-12-22 21:05:58 +0100

    Lectia 17, Events

--- script.js
+++ script.js
@@ -1,225 +1,110 @@
-let a = 10;
-let nume = 'Alexandru';
+let img = document.querySelector('#imagineGalerie');
+let indexText = document.querySelector('#index_img');
 
-// Operatorul de verificare a egalitatii == (egalitate slaba)
-
-// console.log(nume == 'Alexandru')  - true
-
-// console.log(10 == '10');
-
-// Operatorul de verificare egalitatii stricte === (egalitate stricta)
-
-// console.log(10 === '10');
-
-
-// Operatorul de verificare a inegalitatii != (inegalitate slaba)
-
-// console.log(10 != '10');
-
-
-// Operatorul de verificare inegalitatii stricte !== (inegalitate stricta)
-
-// console.log(10 !== '10');
-
-
-// Comparatii:
-
-// >
-// console.log(6 > 5);
-
-// <
-// console.log(6 < 5);
-
-// >=
-// console.log(5 >= 5);
-
-// <=
-// console.log(4 <= 5);
-
-
-
-// OPERATORII LOGICI
-
-// AND (și) -   &&
-
-let email = 'a@b.c';
-
-
-// console.log(username == 'adin' && password == '12345');
-//                 false                   true
-
-
-// OR (sau) - ||
-
-// console.log(username == 'admidwadwadawn' || email == 'a@b.c');
-//                  false                     true
-
-// NOT -   !
-// console.log(!false)
-
-
-// if - else
-let username = 'admin';
-let password = '12345';
-
-
-if (username == 'admin' && password == '12345') {
+let imagini = [
+    /*0*/   'https://i0.wp.com/plopdo.com/wp-content/uploads/2021/07/Screenshot-1.png?resize=1210%2C642&ssl=1',
+    /*1*/   'https://kinsta.com/wp-content/uploads/2021/11/front-end-developer.png',
+    /*2*/   'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg',
+    /*3*/   'https://www.weblineindia.com/wp-content/uploads/2017/04/front-end-development-with-weblineindia-1.jpg',
+    /*4*/   'https://techvify-software.com/wp-content/uploads/2023/07/frontend-development-tools.png',
+    /*5*/   'https://t3.ftcdn.net/jpg/03/18/60/62/360_F_318606217_Hk8jo2MVoI33SQOkYrfOF929J7JgIP0P.jpg',
     
-} else if (username != 'admin' && password == '12345') {
-    console.log('Usernameul este gresit!');
-
-    // prin DOM să facem vizibil în pagină un mesaj de eroare
-} else if (username == 'admin' && password != '12345') {
-    console.log('Parola este gresita!');
-} else {
-    console.log('Username-ul si parola sunt gresite!');
-}
-
-let temperatura = 30;
-
-if (temperatura < 15) {
-    console.log('Afara este frig!');
-} else if (temperatura >=15 && temperatura <= 28) {
-    console.log('Afara temperatura este ok!');
-} else {
-    console.log('Afara este cald!')
-}
-
-
-// switch
-
-let ziSaptamana = 'dwadwadwadadad';
-
-switch (ziSaptamana) {
-    case 'Luni':
-        console.log('Prima zi a saptamanii!');
-        break;
-    
-    case 'Marti':
-        console.log('A doua zi a saptamanii!');
-        break;
-
-    case 'Miercuri':
-        console.log('A treia zi a saptamanii!');
-        break;
-
-    default:
-        console.log('A doua jumatate a saptamanii!');
-
-}
-
-
-let masina = {
-    companie: 'Toyota',
-    motor: {
-        volum: 2.4,
-        cilindri: 4,
-    },
-    manual: false
-
-
-    // Obiectele pot sa contina si functii
-}
-// stergerea unei proprietati
-delete masina.companie;
-
-
-// crearea/modificarea unei proprietati
-masina.model = 'Corolla';
-
-
-
-
-console.log(masina.motor.cilindri);
-
-console.log(console);
-
-
-// Array-uri
-// let nume1 = 'Ion';
-// let nume2 = 'Alexandru';
-// let nume3 = 'Maria';
-
-let lista_nume = ['Ion','Alexandru','Maria'];
-console.log(lista_nume);
-
-console.log(lista_nume[1])
-console.log(lista_nume[2])
-
-// Adaugarea unui element la sfarsitul arrayului
-// lista_nume.push('Vlad');
-
-// Scoaterea unui element de la capatul arrayului
-// lista_nume.pop();
+   ]
+   
+   img.setAttribute('src', imagini[0])
+   indexText.textContent = 1;
+   
+   let imagineCurenta = 0; // - va contine in interiorul acesteia indicele imaginii curente
+   function nextImage() {
+       imagineCurenta++; // 0->1, 1->2 etc
+   
+       // daca ajungem la sfarsitul listei, resetam valoarea la imagineCurenta la 0
+       // if (imagineCurenta == imagini.length) {
+       //     imagineCurenta=0;
+       // }
+   
+       // **
+   
+       imagineCurenta = imagineCurenta % imagini.length;
+   
+       indexText.textContent = imagineCurenta+1;
+   
+   
+       img.setAttribute('src', imagini[imagineCurenta])
+   
+       console.log(imagineCurenta)
+       console.log(img.getAttribute('src'));
+   
+   
+   }
+   
+   
+   
+   function previousImage() {
+   
+       imagineCurenta--; // 3->2, 2->1, 1->0
+   
+       if (imagineCurenta == -1) {
+           imagineCurenta = imagini.length-1;
+       }
+   
+       indexText.textContent = imagineCurenta+1;
+   
+       // ***
+       // imagineCurenta = imagineCurenta % imagini.length;
+   
+       img.setAttribute('src', imagini[imagineCurenta])
+   
+   
+       console.log(imagineCurenta)
+       console.log(img.getAttribute('src'));
+       
+   }
+   
+   function changeImage(actiune) {
+       if (actiune == 'next') {
+   
+       } else if (actiune == 'previous') {
+           
+       }
+   }
+   
+   let container_card = document.querySelector('#container');
+   
+   function addCard() {
+   
+       // Cream cardul
+   
+       let div_card = document.createElement('div');
+       div_card.classList.add('card');
+   
+   
+       // Cream titlul
+       let titlu_card = document.createElement('h1');
+       titlu_card.classList.add('titlu');
+   
+       // Cream descrierea
+       let desc_card = document.createElement('p');
+       desc_card.classList.add('desc');
+       desc_card.textContent = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis inventore quos atque numquam, sequi sit dolore, unde laborum rem nemo impedit molestiae reprehenderit ratione voluptatem voluptatum error minus temporibus ullam.';
+   
+       div_card.append(titlu_card);
+       div_card.append(desc_card);
+   
+       container_card.append(div_card);
 
 
-lista_nume.reverse();
+   }
+   
 
-console.log(lista_nume);
+let paragraf = document.createElement('h4');
+paragraf.textContent = "Frontend Development";
 
-let masini = [
-    {
-        companie: 'Toyota',
-        motor: {
-            volum: 2.4,
-            cilindri: 4,
-        },
-        manual: false
-    },
-    {
-        companie: 'Hyundai',
-        motor: {
-            volum: 2.4,
-            cilindri: 4,
-        },
-        manual: false
-    },
-    {
-        companie: 'BMW',
-        motor: {
-            volum: 2.4,
-            cilindri: 4,
-        },
-        manual: false
-    }
-]
-// let i=5;
-// i++;
-// i++
-// console.log(i); // i = 7;
-// console.log(masini);
-
-let menu_items = ['Transport', 'Imobiliare', 'Aparate telefonice și gadget-uri', 'Calculatoare și birotică', 'Construcții și reparații', 'Haine, încălțăminte și accesorii','Mobilă și interior'];
-
-// menu_items.length - lungimea arrayului
-
-// ciclu for 
-for (let i=0; i<menu_items.length; i++) {
-    // console.log(menu_items[i]);
-}
-
-// ciclu for-of
-
-// optional*** - for-each, for-in
-
-for (const element of menu_items) {
-    console.log(element);
-}
+let box = document.querySelector('.box');
 
 
-// ciclul while
-// let i=0;
-
-// while (i<=10) {
-//     console.log(i);
-
-
-// }
-
-
-// optional*** - do-while
-
-// console.log(i);
+// O functie generala de inserare a elementelor in pagina
+// box.insertAdjacentElement('afterend', paragraf);
 
 
 function greetUser(nume) {
@@ -228,171 +113,144 @@ function greetUser(nume) {
     return 'Bine ai revenit, ' + nume +"!";
 }
 
-lista_nume = ['Ion','Alexandru','Maria','Vlad'];
+// Arrow function (functie sageata)
+let greetUser2 = (nume) => ('Bine ai revenit, ' + nume + '!');
 
-for (const element of lista_nume) {
-    console.log(greetUser(element));
+console.log(greetUser2('Alexandru'));
+
+// let, const
+// var
+
+
+img = document.querySelector('#imagineGalerie');
+
+function moveImage() {
+    let img = document.createElement('img');
+    img.setAttribute('src', 'https://techvify-software.com/wp-content/uploads/2023/07/frontend-development-tools.png');
+
+    document.body.insertAdjacentElement('beforeend',img)
 }
 
+moveImage();
 
-
-// DOM (Document Object Model)
-
-// document.getElementById();
-// document.getElementsByClassName();
-// document.getElementsByTagName();
-
-let descriere = document.querySelector('#descriere');
-
-
-
-// .textContent - utilizată doar pentru conținut textual intern!
-// descriere.textContent =  'Bine ai revenit, <b>Alexandru</b>!'
-
-
-menu_items = ['Transport', 'Imobiliare', 'Aparate telefonice și gadget-uri', 'Calculatoare și birotică', 'Construcții și reparații', 'Haine, încălțăminte și accesorii']
-
-descriere.innerHTML =  'Bine ai revenit, <b>Alexandru</b>!';
-
-let titluri = document.querySelectorAll('.titlu');
-
-// function incarcaMeniu() {
-//     for (let i=0; i<titluri.length; i++) {
-//         // console.log(titluri[i].textContent);
-//         // console.log(menu_items[i])
-    
-//         titluri[i].textContent = menu_items[i];
-//     }
-// }
-
-let carduri = document.querySelectorAll('.card');
-
-
-// function incarcaContinut() {
-//     for (const card of carduri) {
-
-//         // card.classList.add() - adaugare clasa
-//         // card.classList.remove() - scoate clasa
-
-//         card.classList.toggle('d-block');
-//         // card.style.border
-//         // card.style.borderRadius
-//     }
-
-//     for (let i=0; i<titluri.length; i++) {
-//         titluri[i].textContent = menu_items[i];
-//     }
-// }
-
-// function ascundeContinut() {
-//     for (const card of carduri) {
-//         card.classList.remove('d-block');
-//     }
-// }
-
-
-let img = document.querySelector('#imagineGalerie');
-let indexText = document.querySelector('#index_img');
-
-
-
-let imagini = [
- /*0*/   'https://i0.wp.com/plopdo.com/wp-content/uploads/2021/07/Screenshot-1.png?resize=1210%2C642&ssl=1',
- /*1*/   'https://kinsta.com/wp-content/uploads/2021/11/front-end-developer.png',
- /*2*/   'https://t3.ftcdn.net/jpg/02/92/88/72/360_F_292887204_2wH041phSQo70eqaE9GRqFvn5MmQ4B8w.jpg',
- /*3*/   'https://www.weblineindia.com/wp-content/uploads/2017/04/front-end-development-with-weblineindia-1.jpg',
- /*4*/   'https://techvify-software.com/wp-content/uploads/2023/07/frontend-development-tools.png',
- /*5*/   'https://t3.ftcdn.net/jpg/03/18/60/62/360_F_318606217_Hk8jo2MVoI33SQOkYrfOF929J7JgIP0P.jpg',
- 
-]
-
-img.setAttribute('src', imagini[0])
-indexText.textContent = 1;
-
-let imagineCurenta = 0; // - va contine in interiorul acesteia indicele imaginii curente
-function nextImage() {
-    imagineCurenta++; // 0->1, 1->2 etc
-
-    // daca ajungem la sfarsitul listei, resetam valoarea la imagineCurenta la 0
-    // if (imagineCurenta == imagini.length) {
-    //     imagineCurenta=0;
-    // }
-
-    // **
-
-    imagineCurenta = imagineCurenta % imagini.length;
-
-    indexText.textContent = imagineCurenta+1;
-
-
-    img.setAttribute('src', imagini[imagineCurenta])
-
-    console.log(imagineCurenta)
-    console.log(img.getAttribute('src'));
-
-
-}
-
-
-
-function previousImage() {
-
-    imagineCurenta--; // 3->2, 2->1, 1->0
-
-    if (imagineCurenta == -1) {
-        imagineCurenta = imagini.length-1;
+    {
+        var nume = 'Alexandru';
     }
 
-    indexText.textContent = imagineCurenta+1;
-
-    // ***
-    // imagineCurenta = imagineCurenta % imagini.length;
-
-    img.setAttribute('src', imagini[imagineCurenta])
+    console.log(nume);
 
 
-    console.log(imagineCurenta)
-    console.log(img.getAttribute('src'));
-    
+// LOCAL STORAGE SI SESSION STORAGE
+
+if (localStorage.getItem('darkMode')=='true') {
+    bodyDarkMode();
 }
 
-function changeImage(actiune) {
-    if (actiune == 'next') {
 
-    } else if (actiune == 'previous') {
-        
-    }
+
+function bodyDarkMode() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+}
+function bodyWhiteMode() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
 }
 
-let container_card = document.querySelector('#container');
+let butonDarkMode = document.querySelector('#button_dark_mode');
 
-function addCard() {
-
-    // Cream cardul
-
-    let div_card = document.createElement('div');
-    div_card.classList.add('card');
-
-
-    // Cream titlul
-    let titlu_card = document.createElement('h1');
-    titlu_card.classList.add('titlu');
-
-    // Cream descrierea
-    let desc_card = document.createElement('p');
-    desc_card.classList.add('desc');
-    desc_card.textContent = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis inventore quos atque numquam, sequi sit dolore, unde laborum rem nemo impedit molestiae reprehenderit ratione voluptatem voluptatum error minus temporibus ullam.';
-
-    div_card.append(titlu_card);
-    div_card.append(desc_card);
-
-    container_card.append(div_card);
+function setDarkMode() {
+        if (localStorage.getItem('darkMode')=='true'){
+            bodyWhiteMode();
+            localStorage.removeItem('darkMode');
+        } else {
+            localStorage.setItem('darkMode','true');
+            bodyDarkMode();
+        }
 }
 
-/*
- <div class="card">
-    <h1 class="titlu"></h1>
-    <p class="desc"></p>
-</div>
+butonDarkMode.addEventListener("click", setDarkMode);
 
-*/
+
+butonDarkMode.addEventListener('mouseenter', () => {
+    butonDarkMode.classList.add('loadInversat');
+})
+butonDarkMode.addEventListener('mouseleave', () => {
+    butonDarkMode.classList.remove('loadInversat');
+})
+
+
+let textInputBox = document.querySelector('#textInputBox');
+
+// textInputBox.addEventListener('keydown', function() {
+//     textInputBox.style.backgroundColor = 'red';
+// })
+// textInputBox.addEventListener('keyup', function() {
+//     textInputBox.style.backgroundColor = '';
+// })
+
+let formLogin = document.querySelector('#formLogin');
+
+formLogin.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // o functie care trimite datele pe server
+    // 
+    // 
+
+    formLogin.reset();
+
+    let alerta = document.querySelector('#submissionSuccess');
+    alerta.style.opacity = '1';   
+
+})
+
+// focus, blur
+let inputsForm = document.querySelectorAll('.inputForm');
+
+for (const input of inputsForm) {
+    input.addEventListener('focus', function() {
+        input.classList.add('enteredInput');
+    })
+
+    // blur pentru a scoate clasa
+}
+
+// load, unload
+// document.addEventListener('load');
+// document.addEventListener('unload');
+
+
+// Evenimentul scroll
+window.addEventListener('scroll', bodyDarkMode);
+
+
+window.addEventListener('resize', function() {
+    console.log('Ecran Schimbat')
+})
+
+// mousedown, mouseup, mousemove
+
+let passwordInput = document.querySelector('#passwordInput');
+let passwordCount = document.querySelector('#passwordCount');
+
+
+passwordInput.addEventListener('input', function() {
+    passwordCount.textContent = passwordInput.value;
+})
+
+// dblclick
+
+// touchstart, touchend, touchmove
+
+
+// transitioned
+
+
+// offline/online
+// window.addEventListener('online');
+// window.addEventListener('offline');
+
+// hashchange
+
+// window.addEventListener('hashchange');
